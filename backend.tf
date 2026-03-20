6d49402ff3cc74bef16a4b163921fc889ff9a7af
Omodeni Dolapo <109023550+Omodolar@users.noreply.github.com>
2026-03-20 20:54:46 +0100

    added backend for state management

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform  {
+  backend  "s3"  {
+    bucket  =  "dolapo-iac-statefile"
+    key     =  "vpc/prod/terraform.tfstate"
+    region  =  "us-east-1"
+    dynamodb_table  =  "tf-state-lock"
+    encrypt  =  true
+  }
+}
+
