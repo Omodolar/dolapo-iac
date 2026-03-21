0778a70f8884a11b70fa647e87c4aa659024b060
Omodeni Dolapo <109023550+Omodolar@users.noreply.github.com>
2026-03-21 08:23:43 +0100

    Removed lock file

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform  {
-  backend  "s3"  {
+  backend  "s3" {
     bucket  =  "dolapo-iac-statefile"
     key     =  "vpc/prod/terraform.tfstate"
     region  =  "us-east-1"
-    use_lockfile = true
   }
 }
 
